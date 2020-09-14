24bf33a7f8ef61987aedfee53754626fa6b20c3e
Shahrukh Muhammad Qasim <ShahrukhMuhammad.Qasim@gfk.com>
2020-09-14 15:31:31 +0500

    FixNewMethod added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -30,7 +30,10 @@ namespace NSPTestGitProject.Controllers
         {
             return View();// Index
         }
-
+        public IActionResult IssueFix2NewMethod()
+        {
+            return View();// Index
+        }
         public IActionResult Privacy()
         {
             return View();
